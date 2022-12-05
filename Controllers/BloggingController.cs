09890ce61c9c1f8164601f27ba6f9b09fc84f31f
bigegi84 <gilangpw84@gmail.com>
2022-12-05 10:45:13 +0700

    up

---
+++ Controllers/BloggingController.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_api_test.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BloggingController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<BloggingController> _logger;
+
+    public BloggingController(ILogger<BloggingController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "Blogging")]
+    public IEnumerable<Blog> Get()
+    {
+
+        using var db = new BloggingContext();
+
+        // Note: This sample requires the database to be created before running.
+        Console.WriteLine($"Database path: {db.DbPath}.");
+
+        // Create
+        // Console.WriteLine("Inserting a new blog");
+        // db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+        // db.SaveChanges();
+
+        // Read
+        Console.WriteLine("Querying for a blog");
+        var blog = db.Blogs
+            .OrderBy(b => b.BlogId)
+            .ToList();
+        return blog;
+    }
+    [HttpPost(Name = "Blogging")]
+    public IEnumerable<Blog> Post([FromBody] Blog body)
+    {
+
+        using var db = new BloggingContext();
+
+        // Note: This sample requires the database to be created before running.
+        Console.WriteLine($"Database path: {db.DbPath}.");
+
+        // Create
+        Console.WriteLine("Inserting a new blog");
+        db.Add(body);
+        db.SaveChanges();
+
+        // Read
+        Console.WriteLine("Querying for a blog");
+        var blog = db.Blogs
+            .OrderBy(b => b.BlogId)
+            .ToList();
+        return blog;
+    }
+    [HttpPut()]
+    public IEnumerable<Blog> Delete([FromBody] Blog body)
+    {
+
+        using var db = new BloggingContext();
+
+        // Note: This sample requires the database to be created before running.
+        Console.WriteLine($"Database path: {db.DbPath}.");
+
+        // Update
+        Console.WriteLine("Querying for a blog");
+        var blog = db.Blogs
+            .Find(body.BlogId);
+        blog.Url = body.Url;
+        db.SaveChanges();
+        // Read
+        Console.WriteLine("Querying for a blog");
+        var list = db.Blogs
+            .OrderBy(b => b.BlogId)
+            .ToList();
+        return list;
+    }
+    [HttpDelete("{id}")]
+    public IEnumerable<Blog> Delete(int id)
+    {
+
+        using var db = new BloggingContext();
+
+        // Note: This sample requires the database to be created before running.
+        Console.WriteLine($"Database path: {db.DbPath}.");
+
+        // Create
+        Console.WriteLine("Inserting a new blog");
+        // Delete
+        Console.WriteLine("Delete the blog");
+        db.Remove(new Blog { BlogId = id });
+        db.SaveChanges();
+
+        // Read
+        Console.WriteLine("Querying for a blog");
+        var blog = db.Blogs
+            .OrderBy(b => b.BlogId)
+            .ToList();
+        return blog;
+    }
+}
